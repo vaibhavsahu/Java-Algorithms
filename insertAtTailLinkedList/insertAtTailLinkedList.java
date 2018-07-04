25780dd7eebb022f9ed04cdf3329bd61a7eab2fa
vaibhav sahu <vaibh3jec@gmail.com>
2018-07-04 00:25:03 -0600

    insert a node at the end of a singly-linked list

---
+++ insertAtTailLinkedList/insertAtTailLinkedList.java
@@ -0,0 +1,30 @@
+//Write a method to insert a node at the end of a singly-linked list. Return the head of the modified list. 
+
+
+public ListNode insertAtTail(ListNode head, int data) {
+    
+//if list is empty
+    if(head == null){
+        //create new node and mark it as head.
+        ListNode temp = new ListNode(data);
+        temp.next = null;
+        head = temp;
+        return head;
+     }else if(head.next == null)
+     {
+         ListNode temp = new ListNode(data);
+         temp.next = null;
+         head.next = temp;
+         return head;
+     }else{
+         ListNode temp = head;
+         while(temp.next != null){
+             temp = temp.next;
+         }
+         ListNode newNode = new ListNode(data);
+         newNode.next = null;
+         temp.next = newNode;
+         return head;
+     }
+
+}
