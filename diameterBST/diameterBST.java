ae8422355baaea89dac40be04d38176a36d564af
vaibhav sahu <vaibh3jec@gmail.com>
2018-07-03 21:25:51 -0600

    Find the Diameter of a BST

---
+++ diameterBST/diameterBST.java
@@ -0,0 +1,28 @@
+//Find the Diameter of a BST
+
+public int diameter(TreeNode root) {
+    if (root == null)
+            return 0;
+ 
+        /* get the height of left and right sub trees */
+        int lheight = height(root.left);
+        int rheight = height(root.right);
+ 
+        /* get the diameter of left and right subtrees */
+        int ldiameter = diameter(root.left);
+        int rdiameter = diameter(root.right);
+ 
+        /* Return max of following three
+          1) Diameter of left subtree
+         2) Diameter of right subtree
+         3) Height of left subtree + height of right subtree + 1 */
+        return Math.max(lheight + rheight + 1,
+                        Math.max(ldiameter, rdiameter));
+}
+
+public int height(TreeNode root){
+    if(root == null){
+        return 0;
+    }
+    return 1+Math.max(height(root.left), height(root.right));
+}
