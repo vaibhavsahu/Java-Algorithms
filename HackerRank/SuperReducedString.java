6de5284844c057d0a4b8dbca891cc1b141d04849
Vaibhav Sahu <vaibh3jec@gmail.com>
2018-09-16 01:44:07 -0600

    https://www.hackerrank.com/challenges/reduced-string

---
+++ HackerRank/SuperReducedString.java
@@ -0,0 +1,9 @@
+    public static String superReducedString(String str){
+        for (int i = 1; i < str.length(); i++) {
+            if (str.charAt(i) == str.charAt(i-1)) {
+                str = str.substring(0, i-1) + str.substring(i+1);
+                i = 0;
+            }
+        }
+    return str.isEmpty() ? "Empty String" : str;
+    }
