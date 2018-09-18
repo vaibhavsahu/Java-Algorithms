529e5806bab1731c1838589f7437544c4c0ee3f8
Vaibhav Sahu <vaibh3jec@gmail.com>
2018-09-18 13:08:45 -0600

    List to Set
    Java

---
+++ Java8/ListToSet.java
@@ -0,0 +1,4 @@
+    public static void main(String[] args) {
+        List<String> namesList = Arrays.asList( "Red", "Blue", "Green" );
+         Set<String> set = new HashSet<String>(namesList);
+    }
