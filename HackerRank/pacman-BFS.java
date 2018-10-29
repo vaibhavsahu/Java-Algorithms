67756adbe515082519a66c3af3cdcd2b539a288b
Vaibhav Sahu <vaibh3jec@gmail.com>
2018-10-29 01:08:04 -0600

    added equals() for Point objects comparison, added boolean visited matrix

--- HackerRank/pacman-BFS.java
+++ HackerRank/pacman-BFS.java
@@ -1,12 +1,19 @@
-import java.io.*;
-import java.util.*;
-
 class Point{
     int x;
     int y;
+    //int dist;
     Point(int x, int y){
         this.x = x;
         this.y = y;
+        //this.dist = 0;
+    }
+
+    @Override
+    public boolean equals(Object other) {
+        if(this == other) return false;
+        if(!(other instanceof Point)) return false;
+        Point thatPoint = (Point)other;
+        return this.x == thatPoint.x && this.y == thatPoint.y;
     }
 }
 
@@ -15,53 +22,58 @@ class Point{
 //LEFT(i, j) -> (i, j-1)
 //DOWN(i, j) -> (i, j+1)
 
-public class Solution {
-    
+public class Main {
     public static boolean inRange(int i, int j, char [][] matrix){
-        if(i < matrix.length || i > matrix.length || j < matrix[0].length || j > matrix[0].length){
+        if(i < 0 || i > matrix.length || j < 0 || j > matrix[0].length){
             return false;
         }
         return true;
     }
-    
-    
-    public static int BFS(Point source, Point destination, char [][] matrix){
-        
+
+
+    public static int BFS(Point source, Point destination, char [][] matrix) {
+
         LinkedList<Point> queue = new LinkedList();
-        HashSet<Point> visited = new HashSet();
-        
+        boolean [][] visited = new boolean[matrix.length][matrix[0].length];
+
         queue.add(source);
-        
-        
-        while(!queue.isEmpty()){
+        int pathLength = 0;
+
+        while (!queue.isEmpty()) {
             Point node = queue.remove();
-            visited.add(node);
-            
+            if (node.equals(destination)) {
+                System.out.println("path found");
+                return pathLength;
+            }
+            visited[node.x][node.y] = true;
+
             //neighbor list
             ArrayList<Point> neighbors = new ArrayList();
-            if(inRange(node.x-1, node.y) && matrix[node.x-1][node.y] != '%') //UP
-                neighbors.add(new Point(node.x-1, node.y));
-            if(inRange(node.x+1, node.y) && matrix[node.x+1][node.y] != '%') //DOWN
-                neighbors.add(new Point(node.x+1, node.y));
-            if(inRange(node.x, node.y-1) && matrix[node.x][node.y-1] != '%') //LEFT
-                neighbors.add(new Point(node.x, node.y-1));
-            if(inRange(node.x, node.y+1) && matrix[node.x][node.y+1] != '%') //UP
-                neighbors.add(new Point(node.x, node.y+1));
-            
-            for(Point p : neighbors){
-                if(!visited.contains(p)){
-                    
+            if (inRange(node.x - 1, node.y, matrix) && matrix[node.x - 1][node.y] != '%' && !visited[node.x - 1][node.y]) { //UP
+                neighbors.add(new Point(node.x - 1, node.y));
+            }
+            if (inRange(node.x + 1, node.y, matrix) && matrix[node.x + 1][node.y] != '%' && !visited[node.x + 1][node.y]) { //DOWN
+                neighbors.add(new Point(node.x + 1, node.y));
+                //System.out.println("i: " + (node.x + 1) + ", "+ "j: " + node.y);
+            }
+            if (inRange(node.x, node.y - 1, matrix) && matrix[node.x][node.y - 1] != '%' && !visited[node.x][node.y-1]) { //LEFT
+                neighbors.add(new Point(node.x, node.y - 1));
+            }
+            if (inRange(node.x, node.y + 1, matrix) && matrix[node.x][node.y + 1] != '%' && !visited[node.x][node.y+1]) { //DOWN
+                neighbors.add(new Point(node.x, node.y + 1));
+            }
+
+            for (Point p : neighbors) {
+                if (!visited[p.x][p.y]) {
+                    pathLength++;
+                    visited[p.x][p.y] = true;
+                    queue.add(p);
                 }
             }
-            
         }
-        
-        
-        
-        
-        
-        return 0;
+        return pathLength;
     }
+
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
